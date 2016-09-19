4d1135b052070c535d3283e1c515e31b8bee6040
liyuanhong <908963295@qq.com>
2016-09-19 21:48:16 +0800

    first commit

---
+++ application/views/pages/test.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head><title>bootstrap test</title>
+</head>
+<body>
+<link href="<?php echo base_url();?>dist/css/bootstrap.min.css" rel="stylesheet">
+
+<button class="btn btn-lg btn-sucess">test</button>
+
+</body>
+</html>
+</html>
