6bbc5e31a5b4ee965d3b65b8bfe8ec3d3bc8981c
liyuanhong <908963295@qq.com>
2016-09-22 17:55:05 +0800

    add something

--- application/views/pages/test.php
+++ application/views/pages/test.php
@@ -7,8 +7,19 @@
 </head>
 <body>
 <link href="<?php echo base_url();?>static/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo base_url();?>static/devMS/test.css" rel="stylesheet">
 
-<button class="btn btn-lg btn-info">test</button>
+<div id="main">
+	<div id="top_bar"></div>
+	<div id="left_item">
+		<div class="setting_item">
+			<label class="setting_label">设备查询</label>
+		</div>
+	</div>
+	<div id="right_content">
+		 
+	</div>
+</div>
 
 </body>
 </html>
