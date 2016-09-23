cd2ebc9dd3361d52368497c9e11c015c608dfdfa
liyuanhong <908963295@qq.com>
2016-09-23 18:03:40 +0800

    modify a little

--- application/views/showDev/showDevs.php
+++ application/views/showDev/showDevs.php
@@ -9,6 +9,10 @@
 <link href="<?php echo base_url();?>static/devMS/css/showDev/showDevs.css" rel="stylesheet">
 
 <div id="search_area">
+	<select id="dev_brand">
+		<option value="android">android</option>
+		<option value="ios">ios</option>
+	</select>
 </div>
 
 </body>
