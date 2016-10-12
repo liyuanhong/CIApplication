925e7a67314ea3991c2135e4f8c5d79c00358c16
liyuanhong <908963295@qq.com>
2016-10-13 00:01:04 +0800

    database

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '192.168.8.49',
+	'username' => 'root',
+	'password' => 'pass',
+	'database' => 'devManageSYS',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
