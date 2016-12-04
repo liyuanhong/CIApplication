a0a42902fb1e0e000aa804c4c6e95240a37ae976
liyuanhong <908963295@qq.com>
2016-12-04 14:20:53 +0800

    添加了排序功能

--- application/models/ShowDevMod.php
+++ application/models/ShowDevMod.php
@@ -11,7 +11,7 @@ class ShowDevMod extends CI_Model {
 	//条件查询符合要求的设备
 	public function searchDevs($plateform,$brand,$version,$status,$category,$borrower,$old_dev){
 		//$queryString = "select a.id,a.device_name,a.model,a.theNum,a.owner,a.status,a.borrower,a.borrow_time,b.path from devices a,dev_imgs b where a.id=b.device_id";
-		$queryString = "select devices.id,device_name,model,theNum,owner,status,borrower,borrow_time,path,device_id from devices left join dev_imgs on devices.id=dev_imgs.device_id";
+		$queryString = "select devices.id,device_name,model,theNum,owner,status,borrower,borrow_time,path,device_id from devices left join dev_imgs on devices.id=dev_imgs.device_id ";
 		
 		$queryString = $queryString.' where devices.id != "" ';
 		if($plateform == "all"){
@@ -50,10 +50,10 @@ class ShowDevMod extends CI_Model {
 			$queryString = $queryString.' and old_dev="'.$old_dev.'"';
 		}
 		if($borrower == ""){
-			
+			$queryString = $queryString.' ORDER BY add_time DESC';
 		}else{
 			//$queryString = $queryString.' and a.borrower like "%'.$borrower.'%"';
-			$queryString = $queryString.' and borrower like "%'.$borrower.'%"';
+			$queryString = $queryString.' and borrower like "%'.$borrower.'%" ORDER BY add_time DESC';
 		}
 		
 		
