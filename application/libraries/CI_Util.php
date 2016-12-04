55e0b5b8bb89a03c1d87251e5c546ae3026bcb7c
liyuanhong <908963295@qq.com>
2016-12-04 14:26:12 +0800

    添加了排序功能

--- application/libraries/CI_Util.php
+++ application/libraries/CI_Util.php
@@ -1,6 +1,7 @@
 <?php
 function getMemberFromIP() {
 	
+	/**
 	$json = file_get_contents("http://check.yixia.com/check/index.php?fn=fetchuserlist");
 	$members = json_decode($json,true);
 	$ip = $_SERVER['REMOTE_ADDR'];
@@ -11,10 +12,10 @@ function getMemberFromIP() {
 	}
 	return '';
 	
+	*/
 	
 	
-	
-	//return "远洪";
+	return "远洪";
 }
 
 
