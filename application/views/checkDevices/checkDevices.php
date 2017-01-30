d7f23afc75eabc5718e214bbc84d776d61874548
liyuanhong <908963295@qq.com>
2017-01-30 17:37:43 +0800

    修改了不登录也能通过url访问管理页面的bug

--- application/views/checkDevices/checkDevices.php
+++ application/views/checkDevices/checkDevices.php
@@ -1,6 +1,14 @@
 <?php
 require dirname(__FILE__)."/../../libraries/CI_Util.php";
 require dirname(__FILE__)."/../../libraries/CI_Log.php";
+
+$ses = isset($_POST['session'])?$_POST['session']:'';
+$session = "";
+if($ses == ""){
+	echo "请登录...";
+	exit();
+}
+
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 $id = $this->uri->segment(3, 0);
 
