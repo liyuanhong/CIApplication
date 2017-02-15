ff0f19130a5f3e97810ecb087b54f2bda23df249
liyuanhong <908963295@qq.com>
2017-02-15 11:11:19 +0800

    no

--- application/libraries/CI_Util.php
+++ application/libraries/CI_Util.php
@@ -1,19 +1,6 @@
 <?php
 function getMemberFromIP() {
 	
-	/**
-	$json = file_get_contents("http://check.yixia.com/check/index.php?fn=fetchuserlist");
-	$members = json_decode($json,true);
-	$ip = $_SERVER['REMOTE_ADDR'];
-	foreach ($members as $key => $val) {
-			if ($val['ip'] == $ip) {
-					return $val['sname'];
-			}
-	}
-	return '';
-	
-	*/
-	
 	
 	return "远洪";
 }
